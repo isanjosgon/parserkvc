42895acf3e45b5d13098050d03fb16a3dcdca0eb
Isra San Jose Gonzalez <isanjosgon@gmail.com>
2015-11-19 15:31:47 +0100

    Add KCV parser

---
+++ NSObject+Parser.h
@@ -0,0 +1,12 @@
+//
+//  Created by Isra San Jose on 19/11/2015.
+//  Copyright © 2015 Isra San Jose. All rights reserved.
+//
+
+@import Foundation;
+
+@interface NSObject (Parser)
+
+- (void)parseValuesWithDictionary:(NSDictionary *)json;
+
+@end
---
+++ NSObject+Parser.m
@@ -0,0 +1,51 @@
+//
+//  Created by Isra San Jose on 19/11/2015.
+//  Copyright © 2015 Isra San Jose. All rights reserved.
+//
+
+#import "NSObject+Parser.h"
+
+@implementation NSObject (Parser)
+
+- (NSDictionary*)mappingKeyIfNeeded
+{
+    return @{
+             @"mediatype":@"type",
+             @"description":@"summary",
+             @"media":@"content"
+            };
+}
+
+- (void)parseValue:(id)value forKey:(NSString *)key
+{
+    NSString *mappedKey = [[self mappingKeyIfNeeded] valueForKey:key];
+    if (!mappedKey) {
+        mappedKey = key;
+    }
+    
+    if (![self respondsToSelector:NSSelectorFromString(mappedKey)]) {
+        return;
+    }
+    
+    NSError *error = nil;
+    BOOL validated = [self validateValue:&value forKey:mappedKey error:&error];
+    if (validated) {
+        if (value != [NSNull null] && value != nil) {
+            [self setValue:value forKey:mappedKey];
+        } else {
+            [self setNilValueForKey:mappedKey];
+        }
+    } else {
+        NSLog(@"oops! the value for %@ is invalid in %@.",key,[self.class description]);
+    }
+}
+
+- (void)parseValuesWithDictionary:(NSDictionary *)json
+{
+    for (NSString *key in json) {
+        id value = json[key];
+        [self parseValue:value forKey:key];
+    }
+}
+
+@end
